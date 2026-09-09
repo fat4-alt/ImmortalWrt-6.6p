4a60164ecd4fec44d84ab258bb4f82fc4b78d6fe
fat4-alt <346881691@qq.com>
2026-09-09 16:34:58 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -2,7 +2,7 @@
 #sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
-#sed -i 's/luci-theme-argon/luci-theme-bootstrap/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-argon/luci-theme-bootstrap/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-#sed -i 's/ImmortalWrt/FZSWrt/g' package/base-files/files/bin/config_generate
+sed -i 's/LEDE/FZSWrt/g' package/base-files/files/bin/config_generate
